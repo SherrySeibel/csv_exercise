ee6d565c5d4dea3093124c0b6e83497477c16923
Sherry Seibel <sherry.seibel@gmail.com>
2015-02-19 23:00:48 -0500

    Dynamically render a CSV file

--- site/curriculum_maker.rb
+++ site/curriculum_maker.rb
@@ -2,11 +2,23 @@ require "erb"
 require "csv"
 
 class CurriculumMaker
+  include ERB::Util
+
   attr_reader :course_data, :template
 
   def initialize
     @course_data = []
     @template = "Template missing"
+  end
+
+  def add_template(file_path)
+    @template = File.read(file_path)
+  end
+
+  def add_course_data(file_path)
+    courses = CSV.read(file_path)
+    header = courses.shift
+    @course_data << { header: header, courses: courses }
   end
 
   def render
--- site/merge_template.rb
+++ site/merge_template.rb
@@ -2,9 +2,27 @@ require_relative("curriculum_maker")
 
 this_directory = File.dirname(__FILE__)
 site_directory = File.join(this_directory, "site")
-asset_directory = File.join(site_directory, "_assets")
+assets_directory = File.join(site_directory, "_assets")
 target_directory = File.join(site_directory, "programs")
 
+semester_files = ["first_semester.csv", "second_semester.csv"]
+template_file = "graphic_design.html.erb"
+target_file = "graphic-design.htm"
+
 curriculum_maker = CurriculumMaker.new
+
+template_path = File.join(assets_directory, template_file)
+curriculum_maker.add_template(template_path)
+
+semester_files.each do |semester_file|
+  semester_path = File.join(assets_dir, semester_file)
+  curriculum_maker.add_course_data(semester_path)
+end
+
 content = curriculum_maker.render
-puts content
+# puts content
+
+output_path =  File.join(target_directory, target_file)
+File.open(output_path, "w+") do |file|
+  file.write(content)
+end
